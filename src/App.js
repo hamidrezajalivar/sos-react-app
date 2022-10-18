8139e4030af40727ed252549551cb6c293b0e864
hamidrezajalivar <hamidrezajalivar17830@gmail.com>
2022-10-18 12:49:38 +0330

    create component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Route, BrowserRouter as Router, Routes, useSearchParams } from 'react-router-dom'
+
+import Home from './pages/Home';
+import MainLayout from './layout/MainLayout';
+import NotFound from './components/notfound/NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="page-wrapper">
+    <MainLayout>
+        <Routes>
+ 
+            <Route exact path='/' element={<Home />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+    </MainLayout>
+</div>
   );
 }
 
